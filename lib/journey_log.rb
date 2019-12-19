55caf06e87662c3cbde0f31088d286eb32ff8102
cpcwood <cpcwood@outlook.com>
2019-12-19 19:44:55 +0000

    Refactor

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -3,8 +3,8 @@ class JourneyLog
 
   def initialize(journey_class)
     @journey_class = journey_class
-    @current_journey = journey_class.new
     @journeylist = []
+    new_journey
   end
 
   def start(station)
@@ -13,15 +13,26 @@ class JourneyLog
 
   def finish(station)
     @current_journey.add_touch_out(station)
-    @journeylist << @current_journey.journey
-    @current_journey = journey_class.new
+    log_journey
+    new_journey
   end
 
   def journeys
-    @journeylist.map{|journey| journey.clone }
+    @journeylist.map{|journey| journey.clone}
   end
 
   def in_journey?
     @current_journey.show_touch_in != nil
   end
+
+  private
+
+  def new_journey
+    @current_journey = journey_class.new
+  end
+
+  def log_journey
+    @journeylist << @current_journey.journey
+  end
+
 end
